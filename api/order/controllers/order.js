12da9e81826f32f940f6512975134081e9e7495f
yingsongsana <yingsongsana@gmail.com>
2021-02-05 13:32:40 -0500

    Integrate Stripe into Strapi server
    Stripe API key is a "publishable" version. No security issue here

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,8 +1,45 @@
-'use strict';
+"use strict";
 
 /**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
- * to customize this controller
+ * Order.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Order`.
  */
 
-module.exports = {};
+const stripe = require("stripe")("pk_test_51IHYqeCewqdNP93GHJ9BNRltQZ2GXjCMiAzxvX6zLEL32YRIPTVtQtEdkIElwExfNBX1G5TK4GKwOb6YPdjClYpf00Juk8xIS5");
+
+module.exports = {
+  /**
+   * Create a/an order record.
+   *
+   * @return {Object}
+   */
+
+  create: async (ctx) => {
+    const { address, amount, dishes, token, city, state } = JSON.parse(
+      ctx.request.body
+    );
+    const stripeAmount = Math.floor(amount * 100);
+    // charge on stripe
+    const charge = await stripe.charges.create({
+      // Transform cents to dollars.
+      amount: stripeAmount,
+      currency: "usd",
+      description: `Order ${new Date()} by ${ctx.state.user._id}`,
+      source: token,
+    });
+
+    // Register the order in the database
+    const order = await strapi.services.order.create({
+      user: ctx.state.user.id,
+      charge_id: charge.id,
+      amount: stripeAmount,
+      address,
+      dishes,
+      city,
+      state,
+    });
+
+    return order;
+  },
+};
